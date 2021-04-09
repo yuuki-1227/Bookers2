be86480e055d7dd1c3f8015e7f94d495ebb93059
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-09 08:06:36 +0000

    [Update]header,footerテキストレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
   devise_for :users
   root to: 'homes#top'
+  get 'about' => 'homes#about'
 
+  resources :users, only: [:show, :index]
   resources :books, only: [:new, :create, :index, :update, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
