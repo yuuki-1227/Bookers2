6842f8f490f08e49ddbb2810167cd374198df46d
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-10 01:30:16 +0000

    [Add]新規投稿機能、詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,28 @@
 class BooksController < ApplicationController
 
-  def new
-  end
-
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
-  def update
+  def show
+    @book = Book.find(params[:id])
+    @books = Book.all
   end
 
   def destroy
   end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about'
 
   resources :users, only: [:show, :index]
-  resources :books, only: [:new, :create, :index, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
