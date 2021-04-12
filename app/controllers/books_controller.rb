eea9584a1f2e4736b91abbf2e6f7183cafa9f624
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-12 06:57:53 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      redirect_to book_path(@book), notice: 'You have created book successfully'
+    else
+      render action :index
+    end
   end
 
   def destroy
@@ -30,8 +33,11 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      redirect_to book_path(book), notice: 'You have updated book successfully'
+    else
+      render action :index
+    end
   end
 
   private
