a9cc40f147b0b6f021113c5c78a3e953f49ee2fa
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-13 01:11:46 +0000

    [Update]ユーザー編集URLの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully"
+    else
+      render action: :edit
+    end
+  end
+
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 
-  resources :users, only: [:create, :show, :index, :edit]
+  resources :users, only: [:create, :show, :index, :edit, :update]
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
