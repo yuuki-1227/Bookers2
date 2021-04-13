dcab94e1dc808be7db715beb0b363a7b90fd7ece
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-13 00:47:14 +0000

    [Update]プロフィール画像にリンク先設定,about画面のURL設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'users/registrations' , sessions: 'users/sessions',}
   root to: 'homes#top'
-  get 'about' => 'homes#about'
+  get 'home/about' => 'homes#about'
 
-  resources :users, only: [:create, :show, :index]
+  resources :users, only: [:create, :show, :index, :edit]
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
