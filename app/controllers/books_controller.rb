db22fa43f99ef65aae5cc375db479e367fefd730
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-13 06:19:50 +0000

    [Add]アイコン、投稿編集ボタンのリダイレクト先の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+      redirect_to books_path
+    end
   end
 
   def update
